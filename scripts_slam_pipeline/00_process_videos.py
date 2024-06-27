a87c49d2a293336f869368d76037c3d405cc41d5
AlphaGotReal <patrasubham2004@gmail.com>
2024-06-27 14:46:14 +0530

    fixed paths

--- scripts_slam_pipeline/00_process_videos.py
+++ scripts_slam_pipeline/00_process_videos.py
@@ -87,6 +87,7 @@ def main(session_dir):
                     print(f"Skipping {mp4_path.name}, already moved.")
                     continue
 
+                # HEREEEEE
                 start_date = mp4_get_start_datetime(str(mp4_path))
                 meta = list(et.get_metadata(str(mp4_path)))[0]
                 cam_serial = meta['QuickTime:CameraSerialNumber']
